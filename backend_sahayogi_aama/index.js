b34b89efd770a9d987dca440ce9ccc86726ef420
chelsangya <csangya@gmail.com>
2024-07-10 14:12:18 +0545

    securing secret keys

--- backend_sahayogi_aama/index.js
+++ backend_sahayogi_aama/index.js
@@ -30,10 +30,11 @@ app.use(multiparty());
 
 
 cloudinary.config({ 
-  cloud_name: 'dczxrob6w', 
-  api_key: '153278163438971', 
-  api_secret: 'AylcERDx_b0d9n_2y4hsof7v1yE' 
+  cloud_name:dotenv.config().parsed.CLOUD_NAME,
+  api_key:dotenv.config().parsed.API_KEY,
+  api_secret: dotenv.config().parsed.API_SECRET
 });
+AylcERDx_b0d9n_2y4hsof7v1yE
 
 app.use('/api/user',require('./routes/userRoutes'));
 app.use('/api/aama', require('./routes/aamaRoutes'));
