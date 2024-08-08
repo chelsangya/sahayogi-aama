17ed38ff3bc304f2cf9008ed02e52a901c3af072
chelsangya <csangya@gmail.com>
2024-08-08 10:04:31 +0545

    adding sanitization

--- backend_sahayogi_aama/controllers/contactControllers.js
+++ backend_sahayogi_aama/controllers/contactControllers.js
@@ -1,38 +1,44 @@
+const { validationResult } = require('express-validator');
 const contact = require("../model/contactModel");
 
-const createContact = async (req,res) => {
-    console.log(req.body);
-    const {name, email, number, message} = req.body;
+const createContact = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, number, message } = req.body;
     if (!email || !name || !number || !message) {
         return res.json({
             success: false,
             message: "All fields are required !!"
-        })
+        });
     }
+
     try {
         const newContactList = new contact({
             name: name,
             email: email,
             number: number,
             message: message
-        })
+        });
         await newContactList.save();
         res.status(200).json({
             success: true,
             message: "Submitted successfully !!"
-        })
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.status(500).json({
             success: false,
             message: "Something went wrong !!"
-        })
-    }   
-}
+        });
+    }
+};
 
 const getAllContacts = async (req, res) => {
     try {
-        const list = await contact.find().sort({_id: -1});
+        const list = await contact.find().sort({ _id: -1 });
         res.status(200).json({
             success: true,
             message: "Contacts Fetched",
@@ -47,8 +53,7 @@ const getAllContacts = async (req, res) => {
     }
 };
 
-
 module.exports = {
     createContact,
     getAllContacts
-}
+};
