4c0620fb9f295dc4c046f1ce196a999890bbf777
chelsangya <csangya@gmail.com>
2024-08-08 10:23:54 +0545

    database filter

--- backend_sahayogi_aama/database/database.js
+++ backend_sahayogi_aama/database/database.js
@@ -1,12 +1,21 @@
-//import necessary packages
-const mongoose= require('mongoose');
-mongoose.set('sanitizeFilter', true);
-//function to connect database
-const connectDB= ()=>{
-    mongoose.connect(process.env.DB_URL).then(()=>{
-        console.log('Connected to Database');
-    })
-}
+const mongoose = require('mongoose');
 
-//export
-module.exports= connectDB;
+// Enable query filter sanitization to prevent NoSQL injection
+mongoose.set('sanitizeFilter', true);
+
+// Function to connect to the database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to Database');
+  } catch (error) {
+    console.error('Database connection error:', error);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+// Export the connectDB function
+module.exports = connectDB;
