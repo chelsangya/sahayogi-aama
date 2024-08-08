4d3855871b904c5abe7deaffb480eaf86c353c70
chelsangya <csangya@gmail.com>
2024-08-08 10:03:47 +0545

    adding sanitization

--- backend_sahayogi_aama/routes/contactRoutes.js
+++ backend_sahayogi_aama/routes/contactRoutes.js
@@ -1,7 +1,14 @@
-const router = require('express').Router()
-const contactController = require('../controllers/contactControllers')
+const router = require('express').Router();
+const { body } = require('express-validator');
+const contactController = require('../controllers/contactControllers');
 
-router.post('/create', contactController.createContact)
-router.get('/all', contactController.getAllContacts)
+router.post('/create', [
+    body('name').trim().escape(),
+    body('email').isEmail().normalizeEmail(),
+    body('number').trim().escape(),
+    body('message').trim().escape()
+], contactController.createContact);
 
-module.exports = router
+router.get('/all', contactController.getAllContacts);
+
+module.exports = router;
