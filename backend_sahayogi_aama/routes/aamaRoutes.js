8c1f6a69026a5c767e06dbbbbd98dc825638fd08
chelsangya <csangya@gmail.com>
2024-08-08 10:08:28 +0545

    adding sanitization

--- backend_sahayogi_aama/routes/aamaRoutes.js
+++ backend_sahayogi_aama/routes/aamaRoutes.js
@@ -1,11 +1,40 @@
-const router = require('express').Router()
-const aamaControllers = require('../controllers/aamaControllers')
+const router = require('express').Router();
+const { body } = require('express-validator');
+const aamaControllers = require('../controllers/aamaControllers');
 
-router.post('/create', aamaControllers.createAama)
-router.get('/get', aamaControllers.getAama)
-router.put('/updateVerification', aamaControllers.updateAamaVerification)
-router.get('/getById/:id', aamaControllers.getAamaById)
-router.delete('/delete/:id', aamaControllers.deleteAamaById)
-router.put('/update/:id', aamaControllers.updateAama)
+router.post('/create', [
+  body('name').trim().escape(),
+  body('age').isNumeric().withMessage('Age must be a number'),
+  body('time').trim().escape(),
+  body('charge').isNumeric().withMessage('Charge must be a number'),
+  body('experience').trim().escape(),
+  body('speciality').trim().escape(),
+  body('language').trim().escape(),
+  body('description').trim().escape(),
+  body('isVerified').isBoolean().withMessage('isVerified must be a boolean')
+], aamaControllers.createAama);
 
-module.exports = router
+router.get('/get', aamaControllers.getAama);
+
+router.put('/updateVerification', [
+  body('id').trim().escape(),
+  body('isVerified').isBoolean().withMessage('isVerified must be a boolean')
+], aamaControllers.updateAamaVerification);
+
+router.get('/getById/:id', aamaControllers.getAamaById);
+
+router.delete('/delete/:id', aamaControllers.deleteAamaById);
+
+router.put('/update/:id', [
+  body('name').trim().escape(),
+  body('age').isNumeric().withMessage('Age must be a number'),
+  body('time').trim().escape(),
+  body('charge').isNumeric().withMessage('Charge must be a number'),
+  body('experience').trim().escape(),
+  body('speciality').trim().escape(),
+  body('language').trim().escape(),
+  body('description').trim().escape(),
+  body('isVerified').isBoolean().withMessage('isVerified must be a boolean')
+], aamaControllers.updateAama);
+
+module.exports = router;
